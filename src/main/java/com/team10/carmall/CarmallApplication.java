1729bbcbef1671aae26381007cfa69a1a84a81c1
liaoyu <2546284548@qq.com>
2021-07-04 21:51:05 +0800

    1.0.0

---
+++ src/main/java/com/team10/carmall/CarmallApplication.java
@@ -0,0 +1,13 @@
+package com.team10.carmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team10/carmall/CarmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10.carmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
