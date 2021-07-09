2ed44ead5002a67617f4c27074cdc7caab0954f9
liaoyu <2546284548@qq.com>
2021-07-10 00:23:04 +0800

    User

---
+++ src/main/java/com/team10/exception/AccessException.java
@@ -0,0 +1,15 @@
+package com.team10.exception;
+
+/**
+ * @Author LINZHIPIN
+ * @CreateTime 2021/07/09/00009 14:34:34
+ */
+public class AccessException extends Exception {
+	public AccessException() {
+		super();
+	}
+
+	public AccessException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/team10/settings/service/LogService.java
@@ -0,0 +1,8 @@
+package com.team10.settings.service;
+
+/**
+ * @Author LINZHIPIN
+ * @CreateTime 2021/07/09/00009 15:10:08
+ */
+public interface LogService {
+}
